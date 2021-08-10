1c46d7da29c456f818f7a88baf7f35142152e133
Flávio Moraes <flavio-bp@hotmail.com>
2021-08-10 20:12:16 -0300

    Inserções da aula 1

---
+++ src/ApplicationRoutes.js
@@ -0,0 +1,9 @@
+import { Route } from "react-router";
+import { Home } from "./views/Home";
+
+export const ApplicationRoutes = () => (
+    <>
+        <Route exact path="/" component={Home}></Route>
+        <Route></Route>
+    </>
+)
