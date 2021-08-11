0217fae781f1357e2aa50b03319c191ade8c9292
Flávio Moraes <flavio-bp@hotmail.com>
2021-08-10 22:43:06 -0300

    Inserções da aula 3

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,10 @@
-export const Movie = () => (
+export const Movie = (props) => (
     <div className="movie-item">
         <div>
-            <img src="http://placehold.it/150x150" alt=""/>
+            <img src={`https://image.tmdb.org/t/p/w200/${props.movie.poster_path}`} alt=""/>
         </div>
         <div className="movie-excerpt">
-            <h3>Batman</h3>
+            <h3>{props.movie.title}</h3>
             <a className="btn btn-primary" href="#">Ver detalhes</a>
         </div>
     </div>
